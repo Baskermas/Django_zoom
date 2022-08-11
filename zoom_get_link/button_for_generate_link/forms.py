0fe61d6f3ce3a149f0228f85040848be3db57652
Baskermas <ksin96@mail.ru>
2022-08-11 21:44:12 +0300

    The second version.

--- zoom_get_link/button_for_generate_link/forms.py
+++ zoom_get_link/button_for_generate_link/forms.py
@@ -1,6 +1,2 @@
 from django import forms
 
-
-class UserForm(forms.Form):
-    name = forms.CharField()
-    age = forms.IntegerField()
--- zoom_get_link/button_for_generate_link/views.py
+++ zoom_get_link/button_for_generate_link/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import UserForm
 
 
 def index(request):
     if request.method == "POST":
         return HttpResponse("<h2>{0}</h2>".format("There is the link."))
     else:
-        userform = UserForm()
-        return render(request, "index.html", {"form": userform})
+        return render(request, "index.html")
