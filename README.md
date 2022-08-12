41d1f66c3ca7bfd1e683bce8ad33b01e60a9feac
Baskermas <ksin96@mail.ru>
2022-08-12 03:15:31 +0300

    The second version.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Django_get_link
 You just need to make a dir.
 Load all projects files in that dir.
-Install requeuirements.txt mudules.
+Install requirements.txt mudules.
 In project directory you should write down : python manage.py runserver.
 Next you will see your local host address: copy in the Browser.
 Press the button.
--- zoom_get_link/button_for_generate_link/views.py
+++ zoom_get_link/button_for_generate_link/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import re
+from button_for_generate_link import create_the_link
+
 
 
 def index(request):
     if request.method == "POST":
-        return HttpResponse("<h2>{0}</h2>".format("There is the link."))
+        gen_link = create_the_link.createMeeting()
+        gen_link = re.findall(r"https.*?\s", gen_link)
+        link = gen_link[0]
+        return HttpResponse("<h2>The link is: {0}</h2>".format(link))
     else:
         return render(request, "index.html")
